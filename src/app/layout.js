ee8246be0f9c2a309954441501ac605dca5232a0
dmitrijs <petja314@gmail.com>
2024-11-03 18:04:48 +0000

    fixed font commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,14 +3,21 @@ import "../../public/assets/scss/master.scss"
 import "../styles/extra.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import {Kanit} from "@next/font/google";
 import Head from "next/head";
 
 
 
+const kanit = Kanit({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    style: ["normal", "italic"],
+});
+
 export default async function LocaleLayout({ children }) {
 
     return (
-        <html >
+        <html  >
         <Head>
             <link
                 href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
